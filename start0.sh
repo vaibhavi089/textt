5ed46a9549741537bce73dd72ce5b243935fbdd1
vaibhavi089 <vaibhavikatiyar85@gmail.com>
2025-03-08 14:30:17 +0530

    Update start0.sh

--- start0.sh
+++ start0.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker app:api --bind 0.0.0.0:8000 &
-gunicorn -w 4 -b 0.0.0.0:5000 app:app
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker app0:api --bind 0.0.0.0:8000 &
+gunicorn -w 4 -b 0.0.0.0:5000 app0:app
 
